f8002dc93eaa67535d7503e23abe5634c86542be
HuyTran <HuyTran@DESKTOP-HQ0I331>
2018-07-13 00:22:41 -0700

    update test

--- src/test/java/com/example/demo/DuiBackEndServiceApplicationTests.java
+++ src/test/java/com/example/demo/DuiBackEndServiceApplicationTests.java
@@ -2,13 +2,13 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootConfiguration
 @SpringBootTest
-@ContextConfiguration
 public class DuiBackEndServiceApplicationTests {
 
 	@Test
