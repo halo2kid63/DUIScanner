0a32c7856d91db82dd208e7eb7ad623585d8e0b1
HuyTran <HuyTran@DESKTOP-HQ0I331>
2018-07-13 00:59:52 -0700

    Huy Tran: update latest

--- src/main/java/com/duiscanner/core/DUIBackEndServiceApplication.java
+++ src/main/java/com/duiscanner/core/DUIBackEndServiceApplication.java
@@ -4,16 +4,23 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.example.demo.webExtractor.DUIDataExtractor;
 
 @SpringBootApplication
 @EnableScheduling
-public class DUIBackEndServiceApplication {
+public class DUIBackEndServiceApplication extends SpringBootServletInitializer {
 	
 //	@Autowired
 //	static ConfigProperties config;
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DUIBackEndServiceApplication.class);
+	}
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DUIBackEndServiceApplication.class, args);
